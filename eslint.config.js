e6c60765fba533a891226c14e2f20d99857ab0f2
SgtClickClack <julian.g.roberts@gmail.com>
2025-02-03 12:46:03 +1000

    Update ESLint flat config to remove recommended extends and use minimal custom config

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,21 @@
-const recommended = require("eslint/conf/eslint-recommended");
-
 module.exports = [
-  // Include the recommended ESLint config first.
-  recommended,
   {
     languageOptions: {
       parserOptions: {
         ecmaVersion: 2020,
         sourceType: "module"
       },
-      // Define global variables that were previously provided by the "env" key.
       globals: {
-        // Browser globals
         window: "readonly",
         document: "readonly",
-        // Node.js globals
         process: "readonly",
         __dirname: "readonly",
         module: "readonly"
       }
     },
     rules: {
-      // Add your custom rules or overrides here.
-      // For example: "no-console": "warn",
-    },
-  },
+      "no-console": "warn"
+      // Add additional rules as needed.
+    }
+  }
 ]; 
