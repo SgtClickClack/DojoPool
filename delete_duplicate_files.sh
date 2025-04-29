4343bd936dc2d32bfc504768bf2e0f0a007e60b6
Julia <julia@dojopool.com>
2025-04-29 13:31:30 +1000

    chore: Remove single-use cleanup scripts

--- delete_duplicate_files.sh
+++
@@ -1,7 +0,0 @@
-# Remove the legacy backend database file
-rm src/backend/legacy/database.py
-
-# Remove the outdated frontend helpers file
-rm src/frontend/components/utils/helpers.py
-
-echo "Deleted redundant files successfully." 
