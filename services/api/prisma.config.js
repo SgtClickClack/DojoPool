0c932520b53783af90d06d34669d836e60506946
sgtclickclack <sgtclickclack@gmail.com>
2025-09-26 10:28:15 +1000

    fix(prisma): remove invalid prisma.config.js causing build failure

--- services/api/prisma.config.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  schema: path.join(__dirname, '../../packages/prisma/schema.prisma'),
-  migrations: path.join(__dirname, '../../packages/prisma/migrations'),
-};
